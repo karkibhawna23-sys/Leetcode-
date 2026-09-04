5237ddec5f7b4f491fdccd2de8cf66bebd2a82f3
Bhawna Karki <karkibhawna23@gmail.com>
2026-09-04 19:11:16 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (55.29%) - LeetSync

---
+++ 397-integer-replacement/integer-replacement.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int integerReplacement(int n) {
+        long long x = n;
+        int count = 0;
+
+        while (x != 1) {
+            if (x % 2 == 0) {
+                x = x / 2;
+            }
+            else if (x == 3) {
+                x = x - 1;
+            }
+            else if (x % 4 == 1) {
+                x = x - 1;
+            }
+            else {
+                x = x + 1;
+            }
+
+            count++;
+        }
+
+        return count;
+    }
+};
