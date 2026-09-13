08f44b5731c1f2abf925acacd23fbb2ca92c305b
Bhawna Karki <karkibhawna23@gmail.com>
2026-09-13 20:53:00 +0530

    Time: 1418 ms (10.68%) | Memory: 451.1 MB (21.55%) - LeetSync

---
+++ 187-repeated-dna-sequences/repeated-dna-sequences.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        unordered_map<string, int> mp;
+        vector<string> ans;
+
+        for (int i = 0; i + 10 <= s.size(); i++) {
+            string sub = s.substr(i, 10);
+
+            mp[sub]++;
+
+            if (mp[sub] == 2) {
+                ans.push_back(sub);
+            }
+        }
+
+        return ans;
+    }
+};
